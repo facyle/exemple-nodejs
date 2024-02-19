28401a795e5348f5267da91b2bbd6947c79ced26
jdurand <jdurand@facyle.com>
2024-02-19 16:12:10 +0100

    Commit initial

---
+++ index.mjs
@@ -0,0 +1,20 @@
+import cors from "cors";
+import express from "express";
+
+initServer();
+
+export default function initServer() {
+  const app = express();
+
+  app.use(cors());
+
+  app.get("/health", function (req, res) {
+    console.log("/health");
+    const random = Math.random() * 100;
+    res.status(200).send(`ok ${random}`);
+  });
+
+  app.listen(8080, () => {
+    console.log(`🌏 Server is listening as http on port 8080`);
+  });
+}
